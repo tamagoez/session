26ceeca36bb6a707be7109a2b8a347532bb4d25f
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-25 00:19:40 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ export default function Home() {
 
   return (
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      <p className="text-2xl">Session</p>
       {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
     </div>
   )

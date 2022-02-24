23b5d3ccad4aa0e05da8e5c4355c0382da21674d
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-24 19:55:22 +0900

    Create Auth.js

---
+++ src/Auth.js
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import { supabase } from './supabaseClient'
+
+export default function Auth() {
+  const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState('')
+
+  const handleLogin = async (email, password) => {
+    try {
+      setLoading(true)
+      const { error } = await supabase.auth.signIn({ email, password })
+      if (error) throw error
+      alert('Check your email for the login link!')
+    } catch (error) {
+      alert(error.error_description || error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <div className="row flex flex-center">
+      <div className="col-6 form-widget">
+        <h1 className="header">Supabase + React</h1>
+        <p className="description">Sign in with your email or ID</p>
+        <div>
+          <input
+            className="inputField"
+            type="text"
+            placeholder="Your mail or ID"
+            value={acid}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className="inputField"
+            type="password"
+            placeholder="Your password"
+            value={password}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              handleLogin(acid, password)
+            }}
+            className={'button block'}
+            disabled={loading}
+          >
+            {loading ? <span>Loading</span> : <span>Send magic link</span>}
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}

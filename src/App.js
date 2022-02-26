93b185e8369129b7905df178af33d90ded4a730b
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 01:26:36 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,6 @@ import { supabase } from './supabaseClient'
 // import Account from './Account'
 
 export default function Home() {
-  const [session, setSession] = useState(null)
-
-  useEffect(() => {
-    setSession(supabase.auth.session())
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
-
   return (
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
       <p className="text-2xl">Session</p>

9f9be0aad17d2a6881752375fb02190659a275f0
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 01:39:41 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 // import { supabase } from './supabaseClient'
 // import Auth from './Auth'
 // import Account from './Account'
-import useLocation from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 export default function Home() {
   return (

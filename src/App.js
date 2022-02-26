b1f6d7c49087819f0bace4186b490138a26c2a01
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 01:18:09 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css'
 import { useState, useEffect } from 'react'
 import { supabase } from './supabaseClient'
-import Auth from './Auth'
-import Account from './Account'
+// import Auth from './Auth'
+// import Account from './Account'
 
 export default function Home() {
   const [session, setSession] = useState(null)
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
       <p className="text-2xl">Session</p>
-      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
+      {!session ? return ("<a href="login">Login</a>")}
     </div>
   )
 }

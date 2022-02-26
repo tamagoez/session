1ae77c206a6c6b23705c9c8c65382ca36fd0fa5e
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 01:28:39 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
-import { useState, useEffect } from 'react'
-import { supabase } from './supabaseClient'
+// import { useState, useEffect } from 'react'
+// import { supabase } from './supabaseClient'
 // import Auth from './Auth'
 // import Account from './Account'
 

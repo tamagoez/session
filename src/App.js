ed39848f4465a563b0a02b0cbd4d5030cb4bd5c2
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 12:50:20 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import './App.css'
-// import { useState, useEffect } from 'react'
-// import { supabase } from './supabaseClient'
-// import Auth from './Auth'
-// import Account from './Account'
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-
-export default function Home() {
-  return (
-    <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      <p className="text-1xl">{useLocation()}</p>
-      <p className="text-2xl">Session</p>
-      <a href="login">Login</a>
-    </div>
-  )
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import login from './page/login';
+import toppage from './page/top';
+ 
+ 
+class App extends React.Component {
+  render(){
+    return(
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={toppage} />
+          <Route exact path="/login" component={login} />
+        </Switch>
+      </BrowserRouter>
+    );
+  };
 }
+ 
+export default App;

0c26e356fe7b2466e1fb36ae08896ded8911ffec
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 15:42:22 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login';
 import Toppage from './pages/toppage';
- 
+
+// https://zenn.dev/kenara/articles/3677b9a899cfb9
+import NotFound from './components/404'
  
 class App extends React.Component {
   render(){
@@ -10,7 +12,8 @@ class App extends React.Component {
      <BrowserRouter>
        <Routes>
          <Route path="/" element={<Toppage />} />
-         <Route path="/login" element={<Login />} />;
+         <Route path="/login" element={<Login />} />
+         <Route element={<NotFound />} />;
        </Routes>
      </BrowserRouter>
    );

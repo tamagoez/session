e3443f18666f0103002861d093a30f52f21628f1
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 12:52:55 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import login from './page/login';
-import toppage from './page/top';
+import login from './pages/login';
+import toppage from './pages/top';
  
  
 class App extends React.Component {

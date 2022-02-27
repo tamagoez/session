624e4d144c5f9d03f2b1cc11cb6d04162847872c
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 16:06:49 +0900

    Update 404.js

--- src/components/404.js
+++ src/components/404.js
@@ -11,8 +11,12 @@ class NotFound extends React.Component {
   render() {
     return (
       <div className="container" style={{ padding: '50px 0 100px 0' }}>
-        <h1 className="text-5xl">404 - Not Found</h1>
-        <Link to={`/`} className="button block">Back to top page</Link>
+        <div>
+         <h1 className="text-5xl">404 - Not Found</h1>
+        </div>
+        <div>
+         <Link to={`/`} className="button block">Back to top page</Link>
+        </div>
       </div>
     );
   };

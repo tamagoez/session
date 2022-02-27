9316d2b45229f46a9cf4e220a2e9f23e780267be
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 12:55:07 +0900

    Update toppage.js

--- src/pages/toppage.js
+++ src/pages/toppage.js
@@ -4,7 +4,7 @@ import '../App.css'
 // import Auth from './Auth'
 // import Account from './Account'
 
-export default function Home() {
+export default function toppage() {
   return (
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
       <p className="text-2xl">Session</p>

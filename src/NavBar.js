cb58c1fdc09ceba25c9344e7326ecde7d7163b2e
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 14:52:30 +0900

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -13,7 +13,7 @@ export default function NavBar() {
       </button>
     </div>
     <div className="flex-1">
-      <a href="https://web-sessions.vercel.app/" className="btn btn-ghost normal-case text-xl">Sessions</a>
+      <Link to={`/`}><p className="btn btn-ghost normal-case text-2xl">Sessions</p></Link>
     </div>
     <div className="flex-none">
       <button className="btn btn-square btn-ghost">

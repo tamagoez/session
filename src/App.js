4310fe6a6aa9b9c12cb6b576b917d5c58b7e4293
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 20:45:50 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,47 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login';
 import Toppage from './pages/toppage';
 import Signup from './pages/signup';
+import Account from './pages/Account'
 
 // https://zenn.dev/kenara/articles/3677b9a899cfb9
 // https://reffect.co.jp/react/react-router-6
 import NotFound from './components/404'
  
 class App extends React.Component {
-  render(){
-   return (
-     <BrowserRouter>
-       <Routes>
-         <Route path="/" element={<Toppage />} />
-         <Route path="/login" element={<Login />} />
-         <Route path="/signup" element={<Signup />} />
-         <Route path="*" element={<NotFound />} />;
-       </Routes>
-     </BrowserRouter>
-   );
-  };
+  const [session, setSession] = useState(null)
+  useEffect(() => {
+    setSession(supabase.auth.session())
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+  if (!session) {
+   render(){
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Toppage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<NotFound />} />;
+        </Routes>
+      </BrowserRouter>
+    );
+   };
+  } else {
+   render(){
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Account key={session.user.id} session={session} />} />
+          <Route path="/login" element={<Account key={session.user.id} session={session} />} />
+          <Route path="/signup" element={<Account key={session.user.id} session={session} />} />
+          <Route path="*" element={<NotFound />} />;
+        </Routes>
+      </BrowserRouter>
+    );
+   };
+ };
 }
 
 export default App;

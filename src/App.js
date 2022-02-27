c4b5df14500f8dfaa8d5192ab22681279b0a077d
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 21:17:48 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login';
 import Toppage from './pages/toppage';

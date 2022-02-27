23ac075776c4259c00ffa1d0ddc2d1f7dbf11972
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 21:04:25 +0900

    Create App.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,22 @@
-import React,{ useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login';
 import Toppage from './pages/toppage';
-import Signup from './pages/signup';
-import Account from './pages/Account'
 
 // https://zenn.dev/kenara/articles/3677b9a899cfb9
 // https://reffect.co.jp/react/react-router-6
 import NotFound from './components/404'
  
-class App extends React.Component {
-  // https://tyotto-good.com/blog/reaseons-to-use-function-component
-  constructor(props) {
-    super(props);
-  		// thisを用いてstateを参照しなくてはならない
-    this.state = { session: null };
-    this.handleCount = this.handleCount.bind(this);
-  }
- 
-  useEffect(() => {
-    this.setState({ session: supabase.auth.session() })
-    supabase.auth.onAuthStateChange((_event, this.state.session) => {
-       this.setState({ session: session })
-    })
-  }, [])
-  if (!session) {
-   render(){
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Toppage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="*" element={<NotFound />} />;
-        </Routes>
-      </BrowserRouter>
-    );
-   };
-  } else {
-   render(){
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Account key={session.user.id} session={session} />} />
-          <Route path="/login" element={<Account key={session.user.id} session={session} />} />
-          <Route path="/signup" element={<Account key={session.user.id} session={session} />} />
-          <Route path="*" element={<NotFound />} />;
-        </Routes>
-      </BrowserRouter>
-    );
-   };
- };
+export default function App() {
+  render(){
+   return (
+     <BrowserRouter>
+       <Routes>
+         <Route path="/" element={<Toppage />} />
+         <Route path="/login" element={<Login />} />
+         <Route path="*" element={<NotFound />} />;
+       </Routes>
+     </BrowserRouter>
+   );
+  };
 }
-
-export default App;

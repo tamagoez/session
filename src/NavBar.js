0465db943fb0dbd3d1fc968fc86d13352fb0ff31
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 14:15:35 +0900

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,9 @@
 // import './tailwind.css'
 import './index.css'
 
+import React from 'react';              //Reactを読み込んでいる
+import { Link } from 'react-router-dom';// 追加 Linkタブを読み込む
+
 export default function NavBar() {
   return (
     <div className="navbar bg-base-100">

a32265eddd5db761ca9621f371f3d20888a33cc5
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 12:51:30 +0900

    Create toppage.js

---
+++ src/pages/toppage.js
@@ -0,0 +1,14 @@
+import '../App.css'
+// import { useState, useEffect } from 'react'
+// import { supabase } from './supabaseClient'
+// import Auth from './Auth'
+// import Account from './Account'
+
+export default function Home() {
+  return (
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      <p className="text-2xl">Session</p>
+      <a href="login">Login</a>
+    </div>
+  )
+}

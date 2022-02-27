71314f7519f647e71a759f57615bef0e9bc32b13
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-28 01:21:07 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import Signup from './pages/signup';
 
 // https://zenn.dev/kenara/articles/3677b9a899cfb9
 // https://reffect.co.jp/react/react-router-6
-import NotFound from './components/404'
+import NotFound from './components/404';
  
 class App extends React.Component {
   render(){
-   return (
-     <BrowserRouter>
-       <Routes>
-         <Route path="/" element={<Toppage />} />
-         <Route path="/login" element={<Auth type="login" />} />
-         <Route path="/signup" element={<Auth type="signup" />} />
-         <Route path="*" element={<NotFound />} />;
-       </Routes>
-     </BrowserRouter>
-   );
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Toppage />} />
+          <Route path="/login" element={<Auth type="login" />} />
+          <Route path="/signup" element={<Auth type="signup" />} />
+          <Route path="*" element={<NotFound />} />;
+        </Routes>
+      </BrowserRouter>
+    );
   };
 }
 

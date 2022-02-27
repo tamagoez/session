d6436b66e8890c1d71aeecce9c2e9929526f3409
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 14:36:31 +0900

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,7 +2,7 @@
 import './index.css'
 
 import React from 'react';              //Reactを読み込んでいる
-import { Link } from 'react-router-dom';// 追加 Linkタブを読み込む
+// import { Link } from 'react-router-dom';// 追加 Linkタブを読み込む
 
 export default function NavBar() {
   return (

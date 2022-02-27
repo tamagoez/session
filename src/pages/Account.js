b74e12696928bb03731ae602b80f24f4fac611c4
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-28 01:53:31 +0900

    Update Account.js

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { supabase } from '../supabaseClient'
-import { useLocation } from "react-router-dom";
+import { RedirectuseLocation } from "react-router-dom";
 
 function AccountData({ session }) {
   const [loading, setLoading] = useState(true)

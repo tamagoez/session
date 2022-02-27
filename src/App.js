36eeed43720bd9663df01f1bff1261d24c3ee0ae
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 21:22:54 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import React,{ useState, useEffect } from 'react'
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login';
 import Toppage from './pages/toppage';
+import Signup from './pages/signup';
 
 // https://zenn.dev/kenara/articles/3677b9a899cfb9
 // https://reffect.co.jp/react/react-router-6
-import NotFound from './components/404';
+import NotFound from './components/404'
  
-export default function App() {
-  const [session, setSession] = useState(null)
-  useEffect(() => {
-    setSession(supabase.auth.session())
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, []);
- 
+class App extends React.Component {
   render(){
    return (
      <BrowserRouter>
        <Routes>
          <Route path="/" element={<Toppage />} />
          <Route path="/login" element={<Login />} />
-         <Route path="*" element={<NotFound />} />
+         <Route path="/signup" element={<Signup />} />
+         <Route path="*" element={<NotFound />} />;
        </Routes>
      </BrowserRouter>
    );
- };
-};
+  };
+}
+
+export default App;

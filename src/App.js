444debc89457052c8184764c7139e5e7b8e983c7
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-27 12:41:34 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import './App.css'
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 
+const location = useLocation();
 export default function Home() {
-  const location = useLocation();
   return (
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      <p className="text-1xl">{{location}}</p>
+      <p className="text-1xl">{location}</p>
       <p className="text-2xl">Session</p>
       <a href="login">Login</a>
     </div>

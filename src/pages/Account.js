2ddb6342f0099b8f09a136e6e751057df1ea81e6
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-28 15:52:21 +0900

    Update Account.js

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -112,6 +112,7 @@ function AccountData({ session }) {
 
 export default function Account() {
   // const location = useLocation();
+  const history = useHistory();
   const [session, setSession] = useState(null)
   useEffect(() => {
     setSession(supabase.auth.session())

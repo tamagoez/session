baf809e82dcf8b5daba8f6482721cb7faf98b9be
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-01 00:18:58 +0900

    Update toppage.js

--- src/pages/toppage.js
+++ src/pages/toppage.js
@@ -10,7 +10,7 @@ import { Link } from 'react-router-dom';// 追加 Linkタブを読み込む
 class Toppage extends React.Component {
   render() {
     return (
-      <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      <div>
         <div>
         　<h1 className="text-6xl">Sessions</h1>
           <h2 className="description">Way to Collaborate with your friends, grounp and etc.<br />No personal information need, just input your ID(or Email) and password!</h2>

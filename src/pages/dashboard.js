95fdc0e60ed8b65f90101a11f64f5e12e2e6d046
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-01 16:14:54 +0900

    Update dashboard.js

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { supabase } from '../supabaseClient'
 import { Navigate, useNavigate } from 'react-router-dom'
 

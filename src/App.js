64dae9e3ac9b8f875e8980645e95ae8014e3de8d
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-02 20:26:56 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Auth from './pages/Auth';
 import Toppage from './pages/toppage';
 import Account from './pages/Account';
 import Dashboard from './pages/dashboard';
+import Chat from './pages/chat';
 
 // https://zenn.dev/kenara/articles/3677b9a899cfb9
 // https://reffect.co.jp/react/react-router-6
@@ -19,6 +20,8 @@ class App extends React.Component {
           <Route path="/signup" element={<Auth type="signup" />} />
           <Route path="/account" element={<Account />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/app" element={<Dashboard />} />
+          <Route path="/app/chat/channel/:cid" element={<Chat />} />
           <Route path="*" element={<NotFound />} />;
         </Routes>
       </BrowserRouter>

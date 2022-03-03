7440471290f713294055b57cfc4ee73db2b1e6b2
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-04 00:16:44 +0900

    Update toppage.js

--- src/pages/toppage.js
+++ src/pages/toppage.js
@@ -7,16 +7,19 @@ import '../App.css'
 import React from 'react';
 import { Link } from 'react-router-dom';// 追加 Linkタブを読み込む
 
+import { Text, Divider } from '@chakra-ui/react'
+
 class Toppage extends React.Component {
   render() {
     return (
       <div className="container" style={{ padding: '50px 0 100px 0' }}>
         <div>
-        　<h1 className="text-6xl">Sessions</h1>
-          <p className="description">Way to Collaborate with your friends, grounp and etc.<br />No personal information need, just input your ID(or Email) and password!</p>
+        　<Text fontSize='6xl'>Sessions</Text>
+          <Text fontSize='2xl'>Way to Collaborate with your friends, grounp and etc.<br />No personal information need, just input your ID(or Email) and password!</Text>
         </div>
+        <Divider />
         <div>
-          <p>Let's join to your Sessions!</p>
+          <Text fontSize='2xl'>Let's join to your Sessions!</Text>
           <Link to={`/login`} className="button block primary">Open App</Link>
         </div>
       </div>

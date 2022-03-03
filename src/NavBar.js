054eb2ad8c9ff1f604feed2d4550ded5fbb154f2
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-04 00:36:17 +0900

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,5 @@
 // import './tailwind.css'
-import './index.css'
+// import './index.css'
 
 import React from 'react';              //Reactを読み込んでいる
 // import { Link } from 'react-router-dom';// 追加 Linkタブを読み込む

27dec0d1f2b6a8bf21c89c9e39a49bfd58c464fa
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-03 16:08:31 +0900

    Update chat.js

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { supabase } from '../supabaseClient'
 import React from 'react';
 // import { Link, Navigate } from 'react-router-dom';// 追加 Linkタブを読み込む

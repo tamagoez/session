1ddb1dde8ac088d7fd25e98029dddb7471a2376d
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-03 17:09:52 +0900

    Update GetMes.js

--- src/lib/GetMes.js
+++ src/lib/GetMes.js
@@ -6,9 +6,7 @@ export default function GetMes(props) {
   const [messages, setMessages] = useState([])
   const [users] = useState(new Map())
   const [newMessage, handleNewMessage] = useState(null)
-  const [newChannel, handleNewChannel] = useState(null)
   const [newOrUpdatedUser, handleNewOrUpdatedUser] = useState(null)
-  const [deletedChannel, handleDeletedChannel] = useState(null)
   const [deletedMessage, handleDeletedMessage] = useState(null)
   
   // Get Channels
@@ -22,22 +20,8 @@ export default function GetMes(props) {
       .on('INSERT', (payload) => handleNewMessage(payload.new))
       .on('DELETE', (payload) => handleDeletedMessage(payload.old))
       .subscribe()
-    // Listen for changes to our users
-    const userListener = supabase
-      .from('users')
-      .on('*', (payload) => handleNewOrUpdatedUser(payload.new))
-      .subscribe()
-    // Listen for new and deleted channels
-    const channelListener = supabase
-      .from('channels')
-      .on('INSERT', (payload) => handleNewChannel(payload.new))
-      .on('DELETE', (payload) => handleDeletedChannel(payload.old))
-      .subscribe()
-    // Cleanup on unmount
     return () => {
       messageListener.unsubscribe()
-      userListener.unsubscribe()
-      channelListener.unsubscribe()
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
@@ -61,29 +45,9 @@ export default function GetMes(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [deletedMessage])
 
-  // New channel recieved from Postgres
-  useEffect(() => {
-    if (newChannel) setChannels(channels.concat(newChannel))
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [newChannel])
-
-  // Deleted channel received from postgres
-  useEffect(() => {
-    if (deletedChannel) setChannels(channels.filter((channel) => channel.id !== deletedChannel.id))
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [deletedChannel])
-
-  // New or updated user recieved from Postgres
-  useEffect(() => {
-    if (newOrUpdatedUser) users.set(newOrUpdatedUser.id, newOrUpdatedUser)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [newOrUpdatedUser])
-
   return {
     // We can export computed values here to map the authors to each message
     messages: messages.map((x) => ({ ...x, author: users.get(x.user_id) })),
-    channels: channels !== null ? channels.sort((a, b) => a.slug.localeCompare(b.slug)) : [],
-    users,
   }
 }
 

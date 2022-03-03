c0871f674600b558b206ceff0e1fdae2fcc507a1
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-03 22:05:38 +0900

    Update Message.js

--- src/lib/Message.js
+++ src/lib/Message.js
@@ -3,14 +3,20 @@ import { supabase } from '../supabaseClient'
 
 function Getmes(props) {
   const [ mes, setMes ] = useState([])
-  const getting = supabase
-    .from('channels_chat')
-    .on('INSERT', payload => {
-      console.log('Change received!', payload)
-      setMes(payload)
-    })
-    .subscribe()
-  getting();
+  useEffect(() => {
+    const getting = supabase
+      .from('channels_chat')
+      .on('INSERT', payload => {
+        console.log('Change received!', payload)
+        setMes(payload)
+      })
+      .subscribe()
+  }
+  
+  return {
+    // We can export computed values here to map the authors to each message
+    messages: messages.map((x) => ({ ...x, author: users.get(x.user_id) })),
+  }
 }
 
 function Delmes(props) {

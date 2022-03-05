5779ad08902e34576bba1358634549b170b1ee22
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-05 21:22:05 +0900

    Update toppage.js

--- src/pages/toppage.js
+++ src/pages/toppage.js
@@ -14,12 +14,12 @@ class Toppage extends React.Component {
     return (
       <div className="container" style={{ padding: '50px 0 100px 0' }}>
         <Stack>
-          <Box>
+          <Box p={2}>
            <Text fontSize='6xl'>Sessions</Text>
             <Text fontSize='2xl'>Way to Collaborate with your friends, grounp and etc.<br />No personal information need, just input your ID(or Email) and password!</Text>
           </Box>
           <Divider />
-          <Box>
+          <Box p={2}>
             <Text fontSize='2xl'>Let's join to your Sessions!</Text>
             <LinkBox bg='green.100' maxW='sm' p='3' borderWidth='1px' rounded='md'>
               <LinkOverlay href='/dashboard'>

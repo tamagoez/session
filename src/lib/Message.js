bfd66acd17462a61e3a9133c812c7ef75916b243
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-05 21:01:36 +0900

    Update Message.js

--- src/lib/Message.js
+++ src/lib/Message.js
@@ -4,15 +4,15 @@ import { supabase } from '../supabaseClient'
 function Getmes(props) {
   const [ mes, setMes ] = useState([])
   useEffect(() => {
-    supabase
-      .from('channels_chat')
-      .on('INSERT', payload => {
-        console.log('Change received!', payload)
-        setMes(payload)
-      .eq('id', props.id)
-      })
+    const messageListener = supabase
+      .from('messages')
+      .on('INSERT', (payload) => handleNewMessage(payload.new))
+      .on('DELETE', (payload) => handleDeletedMessage(payload.old))
       .subscribe()
-    console.log('mes: ' + mes)
+    // Cleanup on unmount
+    return () => {
+      messageListener.unsubscribe()
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
   

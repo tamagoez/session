df93aa118625045594ee6d6529efd4662ea3986a
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-12 14:22:48 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,17 +15,16 @@ import { supabase } from './supabaseClient'
 export default function App() {
   const [sessioncount, setSessionCount] = React.useState(0)
   React.useEffect(() => {
-    // setSession(supabase.auth.session())
     supabase.auth.onAuthStateChange((_event, session) => {
       setSessionCount((sessioncount += 1))
       if (sessioncount === 1) {
-       console.log('Seems session change: ' + session)
-       window.location.reload()
+       console.log('Seems session change: ' + session);
+       window.location.reload();
       } else {
-       console.log('Seems this is first render process.: Ignored');
-    })
+       console.log('Seems this is first render process: Ignored');
+      })
    // eslint-disable-next-line react-hooks/exhaustive-deps
- }, []);
+  }, []);
 
   return (
     <BrowserRouter>

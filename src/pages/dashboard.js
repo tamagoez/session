798f720172557b5521138d5331fc7ecb049153c9
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-12 14:39:59 +0900

    Update dashboard.js

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -54,7 +54,7 @@ export default function Dashboard() {
   const session = supabase.auth.session();
   return (
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      {!session ? <Navigate to="/login" state="/dashboard" /> : <MainApp key={session.user.id} session={session} />}
+      {!session ? location.replace("/login?after=/dashboard") /> : <MainApp key={session.user.id} session={session} />}
     </div>
   )
 }

bec2468dac76d0368180df6ca3e49d55902dab02
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-12 15:06:43 +0900

    Create Logout.js

---
+++ src/pages/Logout.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import { supabase } from '../supabaseClient'
+
+// import { useAlert } from 'react-alert'
+
+import React from 'react';
+import LogoutToast from '../components/LogoutToast'
+
+import { Spinner } from '@chakra-ui/react'
+
+function LogoutProcess() {
+  supabase.auth.signOut()
+  const sessioncheck = supabase.auth.session();
+  if (!sessioncheck) {
+    LogoutToast()
+    window.location.replace("/thanks")
+  } else { AuthToast('Signout Failed', 'Something error happend. Please reload this page.', 'error') }
+}
+  
+
+export default function Logout() {
+  const session = supabase.auth.session();
+  // const alert = useAlert();
+  return (
+    <div>
+      <p>Dealing Logout process...<br />After a second, you will redirect to thanks page.</p>
+      <Spinner
+        thickness='4px'
+        speed='0.4s'
+        emptyColor='gray.100'
+        color='green.500'
+        size='xl'
+      />
+      {!session ? window.location.replace("/login?after=/logout") : <LogoutProcess />}
+    </div>
+  )
+}
+
+  
+function AuthToast(title, description, status){
+  // const toast = useToast()
+  const toast = createStandaloneToast()
+  var closab = ((status === 'error') ? false : true)
+  toast({
+        title: title,
+        description: description,
+        status: status,
+        duration: 6000,
+        isClosable: closab,
+      })
+}

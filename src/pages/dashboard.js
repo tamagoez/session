c43fd80dc70694ea1e3104036913e6664c52b01e
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-12 14:50:41 +0900

    Update dashboard.js

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { supabase } from '../supabaseClient'
-import { Navigate, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 function MainApp({ session }) {
   const user = supabase.auth.user();

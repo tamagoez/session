4f5b7e39d16908e94c3af089455e0aad82386189
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-13 16:11:11 +0900

    Update chat.js

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import { supabase } from '../supabaseClient'
 import React from 'react';
 // import { Link, Navigate } from 'react-router-dom';// 追加 Linkタブを読み込む
-import { Navigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 // import { MdPassword, MdAlternateEmail } from "react-icons/md";
 
 // import { GetLog } from '../lib/GetLog';
 // import { CheckRole } from '../lib/CheckRole';
 
-import { Submes } from '../lib/Message';
+import { Submes, Addmes } from '../lib/Message';
 import { Textarea, Button } from '@chakra-ui/react';
 import AlertToast from '../components/AlertToast'
 
@@ -16,74 +16,30 @@ import { IoSend } from "react-icons/io5";
 import BeatLoader from "react-spinners/BeatLoader";
 
 export default function Chat(props) {
+  const navigate = useNavigate();
   var session = supabase.auth.session();
-  const { cid } = useParams();
-  return (
-    <div>
-      {!session ? <Navigate to="/login" state={'/app/chat/' + cid} /> : <CoreChat chid={cid} />}
-    </div>
-  )
+  if (!session) {
+    navigate('/login?after=/app/chat' + cid)
+  } else {
+    const { cid } = useParams();
+    const chid = cid
+    console.log('channelID: ' + chid)
+    var session = supabase.auth.session();
+    const userid = session.user.id;
+    console.log('userID: ' + userid)
+    CheckRole()
+    function CheckRole() {
+      console.log('id is ' + userid)
+    } 
+    return (
+      <div>
+        <CoreChat chid={cid} />}
+      </div>
+    )
+  }
 }
 
 function CoreChat(props) {
-  // Prepare
-  // const [loading, setLoading] = useState(false)
-  // console.log('Loading: ' + loading)
-  // setLoading(true)
-  
-  const chid = props.chid
-  console.log('channelID: ' + chid)
-  var session = supabase.auth.session();
-  
-  // console.log('User session: ' + session)
-  
-  const userid = session.user.id;
-  console.log('userID: ' + userid)
-  
-  // const navigate = useNavigate();
-  // setLoading(false)
-  
-  // useEffect(() => {
-  //   console.log('Load: ' + loading)
-  // }, [loading])
-  
-  CheckRole()
-  // GetLog()
-  
-  // const { messages } = Getmes({ chid })
-  
-  function CheckRole() {
-    console.log('id is ' + userid)
-  } 
-  return (AppProcess(chid));
-}
-
-function AppProcess(chid){
-  const [sendtext, setSendText] = useState(null)
-  const [sendstatus, setSendStatus] = useState(false)
-    
-  const sendMes = async () => {  
-    const user = supabase.auth.user()
-    setSendStatus(true);
-    console.log('Sending message: ' + sendtext);
-    async function senddeal(){
-      try {
-        const { error, status } = await supabase
-          .from('channels_chat')
-          .insert([
-            { userid: user.id, message: sendtext, channel: chid }
-          ],{ upsert: false })
-        if (error && status !== 406) {
-          throw error
-        }
-      } catch (error) {
-        AlertToast('ERROR', error.message, 'error', 6000)
-      } finally {
-        setSendStatus(false)
-      }
-    }
-    senddeal();
-  }
     function MessageBox(){
       return (
         <>
@@ -97,14 +53,13 @@ function AppProcess(chid){
             isLoading={sendstatus}
             colorScheme='green'
             spinner={<BeatLoader size={8} color='white' />}
-            onClick={() => sendMes()}
+            onClick={() => <Addmes text={sendtext} />}
           >
             <IoSend />
           </Button>
         </>
       )
   }
-
   return (
     <div>
       <Submes id={chid} />

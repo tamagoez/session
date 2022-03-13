6dc2b37e8d551336a7ed2fda9dbbf050fb4ecfb1
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-13 10:57:15 +0900

    Update Message.js

--- src/lib/Message.js
+++ src/lib/Message.js
@@ -7,17 +7,10 @@ function Getmes(props) {
   const [messages, setMessages] = useState([])
   const [newMessage, handleNewMessage] = useState(null)
   const [deletedMessage, handleDeletedMessage] = useState(null)
-  const [newOrUpdatedUser, handleNewOrUpdatedUser] = useState(null)
   // const [newChannel, handleNewChannel] = useState(null)
   // const [ mes, setMes ] = useState([]);
-  const [users] = useState(new Map())
   
   useEffect(() => {
-    // Listen for changes to our users
-    const userListener = supabase
-      .from('profiles')
-      .on('*', (payload) => handleNewOrUpdatedUser(payload.new))
-      .subscribe()
     const messageListener = supabase
       .from('channels_chat')
       .on('INSERT', (payload) => handleNewMessage(payload.new))
@@ -26,7 +19,6 @@ function Getmes(props) {
     // Cleanup on unmount
     return () => {
       messageListener.unsubscribe()
-      userListener.unsubscribe()
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
@@ -35,8 +27,6 @@ function Getmes(props) {
   useEffect(() => {
     if (newMessage && newMessage.channel === Number(props.chid)) {
       const handleAsync = async () => {
-        let authorId = newMessage.created_by
-        if (!users.get(authorId)) await fetchUser(authorId, (user) => handleNewOrUpdatedUser(user))
         setMessages(messages.concat(newMessage))
       }
       console.log("New message: " + newMessage)
@@ -50,12 +40,7 @@ function Getmes(props) {
     if (deletedMessage) setMessages(messages.filter((message) => message.id !== deletedMessage.id))
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [deletedMessage])
-  
-  // New or updated user recieved from Postgres
-  useEffect(() => {
-    if (newOrUpdatedUser) users.set(newOrUpdatedUser.id, newOrUpdatedUser)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [newOrUpdatedUser])
+ 
 
   console.log(messages)
   return (
@@ -69,17 +54,6 @@ function Delmes(props) {
 
 function Engmes(props) {
   
-}
-
-export const fetchUser = async (userId, setState) => {
-  try {
-    let { body } = await supabase.from('profiles').select(`*`).eq('id', userId)
-    let user = body[0]
-    if (setState) setState(user)
-    return user
-  } catch (error) {
-    console.log('error', error)
-  }
 }
 
 /**

52217bc0058ec217129a647ed855baeb1ddbf1d0
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-13 16:58:48 +0900

    Update chat.js

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -35,6 +35,7 @@ export default function Chat(props) {
     return () => {
       console.log('Component unmounted')
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
     
   return (
